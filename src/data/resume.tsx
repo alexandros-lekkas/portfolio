daa95e8bb0687696a7a09f41eee101b73ddbd084
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-01-20 12:31:35 -0600

    Update resume data: modify project details, add new projects, and adjust technologies

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -117,7 +117,7 @@ export const DATA = {
     {
       title: "HaulBuy (WIP)",
       href: "https://www.haulbuy.com",
-      dates: "Jan 2024 - Feb 2024",
+      dates: "Jan 2024 - Present",
       active: true,
       description:
         "Currently developing a new ecommerce sourcing platform, allowing users to source products from China they otherwise couldn't. Currently building the client side with WeWeb and the administation panel with Next.js.",
@@ -136,33 +136,48 @@ export const DATA = {
         },
       ],
       image: "https://i.gyazo.com/e90d05154e6502af22d53d708e9d7d83.gif",
-      video:
-        "",
+      video: "",
     },
     {
-      title: "IntelPol",
-      href: "https://www.haulbuy.com",
-      dates: "Jan 2024 - Feb 2024",
+      title: "IB Internal Assessment - Valuator",
+      href: "https://github.com/alexandros-lekkas/ib-ia-valuator",
+      dates: "2023",
       active: true,
       description:
-        "Currently developing a new ecommerce sourcing platform, allowing users to source products from China they otherwise couldn't. Currently building the client side with WeWeb and the administation panel with Next.js.",
+        "A program to calculate company values and simulate mergers, an IB Internal Assessment project.",
       technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Supabase",
-        "WeWeb",
+        "Java",
+        "Java Swing",
+        "Waterfall Development",
+        "Stakeholder Management",
       ],
       links: [
         {
-          type: "Website",
-          href: "https://www.haulbuy.com",
-          icon: <Icons.globe className="size-3" />,
+          type: "GitHub",
+          href: "https://github.com/alexandros-lekkas/ib-ia-valuator/tree/master",
+          icon: <Icons.github className="size-3" />,
         },
       ],
-      image: "https://i.gyazo.com/e90d05154e6502af22d53d708e9d7d83.gif",
-      video:
-        "",
+      image: "https://i.gyazo.com/04fbe60b5092b18fa76373c8a867c472.gif",
+      video: "",
+    },
+    {
+      title: "IB EE - OpenCV Facial Recognition",
+      href: "https://github.com/alexandros-lekkas/ibee-opencv-facial-recognition",
+      dates: "2023",
+      active: true,
+      description:
+        "Legacy facial recognition using the OpenCV library. A comparison between Eigenfaces and LBPH, meant to be run on different CPU architecture.",
+      technologies: ["Python", "OpenCV", "Machine Learning"],
+      links: [
+        {
+          type: "GitHub",
+          href: "https://github.com/alexandros-lekkas/ibee-opencv-facial-recognition",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "",
     },
   ],
 } as const;
