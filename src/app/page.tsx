90faf9a20c2f07eeb9f42e9862c9c17d4bbb611e
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-01-20 11:42:01 -0600

    Add imports for Link and Markdown; improve page component documentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+
+import Markdown from "react-markdown";
+
 import { HackathonCard } from "@/components/hackathon-card";
 import BlurFade from "@/components/magicui/blur-fade";
 import BlurFadeText from "@/components/magicui/blur-fade-text";
@@ -5,12 +9,16 @@ import { ProjectCard } from "@/components/project-card";
 import { ResumeCard } from "@/components/resume-card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
-import { DATA } from "@/data/resume";
-import Link from "next/link";
-import Markdown from "react-markdown";
+
+import { DATA } from "@/data/resume"; // TODO: Remove this and make the page better organized
 
 const BLUR_FADE_DELAY = 0.04;
 
+/**
+ * The home page component. Special thanks to MagicUI for the base design!
+ * 
+ * @returns The home page component.
+ */
 export default function Page() {
   return (
     <main className="flex flex-col min-h-[100dvh] space-y-10">
