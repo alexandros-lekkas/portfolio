7870a37f8270e9be5e4f59dfa22633797b4ef9f7
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-03-14 10:17:05 -0500

    feat: Enhance Hero component with dynamic data and improve layout structure

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -4,6 +4,12 @@ import { BlurFade } from "@/components/magicui/blur-fade";
 
 const BLUR_FADE_DELAY = 0.04;
 
+const data = {
+  name: "Alexandros",
+  description: "Aspiring Software Engineer & Entrepreneur",
+  avatar: "/me.jpeg",
+};
+
 export function Hero() {
   return (
     <section id="hero">
@@ -14,17 +20,17 @@ export function Hero() {
               delay={BLUR_FADE_DELAY}
               className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
               yOffset={8}
-              text={`Hi, I'm Alexandros} 👋`}
+              text={`Hi, I'm ${data.name} 👋`}
             />
             <BlurFadeText
               className="max-w-[600px] md:text-xl"
               delay={BLUR_FADE_DELAY}
-              text={"she skibidi on my toilet until I"}
+              text={data.description}
             />
           </div>
           <BlurFade delay={BLUR_FADE_DELAY} inView>
             <Avatar className="size-28 border">
-              <AvatarImasge alt="Alexandros Lekkas" src={"/me.jpeg"} />
+              <AvatarImage alt="Alexandros Lekkas" src={data.avatar} />
               <AvatarFallback>AL</AvatarFallback>
             </Avatar>
           </BlurFade>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,43 @@
+import Markdown from "react-markdown";
 import Link from "next/link";
 
-import Markdown from "react-markdown";
-
-import BlurFadeText from "@/components/magicui/blur-fade-text";
 import { ProjectCard } from "@/components/data/project-card";
 import { ResumeCard } from "@/components/data/resume-card";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
+
+import { Hero } from "./hero";
 
 import { DATA } from "@/data/resume";
 
 export default function Page() {
   return (
-    <main className="flex flex-col min-h-[100dvh] space-y-10">
-      
+    <div className="flex flex-col min-h-[100dvh] space-y-10">
+      <Hero />
 
       {/* About */}
       <section id="about">
-          <h2 className="text-xl font-bold">About</h2>
-          <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
-            {DATA.summary}
-          </Markdown>
+        <h2 className="text-xl font-bold">About</h2>
+        <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
+          {DATA.summary}
+        </Markdown>
       </section>
 
       {/* Work */}
       <section id="work">
         <div className="flex min-h-0 flex-col gap-y-3">
-            <h2 className="text-xl font-bold">Work Experience</h2>
+          <h2 className="text-xl font-bold">Work Experience</h2>
           {DATA.work.map((work, id) => (
-              <ResumeCard
-                key={work.company}
-                logoUrl={work.logoUrl}
-                altText={work.company}
-                title={work.company}
-                subtitle={work.title}
-                href={work.href}
-                badges={work.badges}
-                period={`${work.start} - ${work.end ?? "Present"}`}
-                description={work.description}
-              />
+            <ResumeCard
+              key={work.company}
+              logoUrl={work.logoUrl}
+              altText={work.company}
+              title={work.company}
+              subtitle={work.title}
+              href={work.href}
+              badges={work.badges}
+              period={`${work.start} - ${work.end ?? "Present"}`}
+              description={work.description}
+            />
           ))}
         </div>
       </section>
@@ -46,17 +45,17 @@ export default function Page() {
       {/* Education */}
       <section id="education">
         <div className="flex min-h-0 flex-col gap-y-3">
-            <h2 className="text-xl font-bold">Education</h2>
+          <h2 className="text-xl font-bold">Education</h2>
           {DATA.education.map((education, id) => (
-              <ResumeCard
-                key={education.school}
-                href={education.href}
-                logoUrl={education.logoUrl}
-                altText={education.school}
-                title={education.school}
-                subtitle={education.degree}
-                period={`${education.start} - ${education.end}`}
-              />
+            <ResumeCard
+              key={education.school}
+              href={education.href}
+              logoUrl={education.logoUrl}
+              altText={education.school}
+              title={education.school}
+              subtitle={education.degree}
+              period={`${education.start} - ${education.end}`}
+            />
           ))}
         </div>
       </section>
@@ -64,10 +63,10 @@ export default function Page() {
       {/* Skills */}
       <section id="skills">
         <div className="flex min-h-0 flex-col gap-y-3">
-            <h2 className="text-xl font-bold">Skills</h2>
+          <h2 className="text-xl font-bold">Skills</h2>
           <div className="flex flex-wrap gap-1">
             {DATA.skills.map((skill, id) => (
-                <Badge key={skill}>{skill}</Badge>
+              <Badge key={skill}>{skill}</Badge>
             ))}
           </div>
         </div>
@@ -76,37 +75,38 @@ export default function Page() {
       {/* Projects */}
       <section id="projects">
         <div className="space-y-12 w-full py-12">
-            <div className="flex flex-col items-center justify-center space-y-4 text-center">
-              <div className="space-y-2">
-                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                  My Projects
-                </div>
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  Check out my latest work
-                </h2>
-                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  I&apos;ve worked on a variety of projects, from simple Java
-                  applications to full-stack web applications. Here are some of
-                  my favorites.
-                </p>
-                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                Some projects that are under wraps are not listed here, so this part may look a little bare (for now). 😅🤫
-                </p>
+          <div className="flex flex-col items-center justify-center space-y-4 text-center">
+            <div className="space-y-2">
+              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                My Projects
               </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                Check out my latest work
+              </h2>
+              <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                I&apos;ve worked on a variety of projects, from simple Java
+                applications to full-stack web applications. Here are some of my
+                favorites.
+              </p>
+              <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Some projects that are under wraps are not listed here, so this
+                part may look a little bare (for now). 😅🤫
+              </p>
             </div>
+          </div>
           <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
             {DATA.projects.map((project, id) => (
-                <ProjectCard
-                  href={project.href}
-                  key={project.title}
-                  title={project.title}
-                  description={project.description}
-                  dates={project.dates}
-                  tags={project.technologies}
-                  image={project.image}
-                  video={project.video}
-                  links={project.links}
-                />
+              <ProjectCard
+                href={project.href}
+                key={project.title}
+                title={project.title}
+                description={project.description}
+                dates={project.dates}
+                tags={project.technologies}
+                image={project.image}
+                video={project.video}
+                links={project.links}
+              />
             ))}
           </div>
         </div>
@@ -115,26 +115,26 @@ export default function Page() {
       {/* Contact */}
       <section id="contact">
         <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
-            <div className="space-y-3">
-              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                Contact
-              </div>
-              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                Get in Touch
-              </h2>
-              <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                Want to chat?{" "}
-                <Link
-                  href={"mailto:alexandros.lekkas@outlook.com"}
-                  className="text-blue-500 hover:underline"
-                >
-                  Just shoot me an email
-                </Link>{" "}
-                and I&apos;ll respond whenever I can.
-              </p>
+          <div className="space-y-3">
+            <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+              Contact
             </div>
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+              Get in Touch
+            </h2>
+            <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+              Want to chat?{" "}
+              <Link
+                href={"mailto:alexandros.lekkas@outlook.com"}
+                className="text-blue-500 hover:underline"
+              >
+                Just shoot me an email
+              </Link>{" "}
+              and I&apos;ll respond whenever I can.
+            </p>
+          </div>
         </div>
       </section>
-    </main>
+    </div>
   );
 }
