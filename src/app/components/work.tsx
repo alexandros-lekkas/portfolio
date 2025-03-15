5af3c260042eddc0c114bf6bb4b3b30085d61bca
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-03-15 16:26:13 -0500

    feat: Refactor Work component to utilize LinkedIn profile data and remove unused images

--- src/app/components/work.tsx
+++ src/app/components/work.tsx
@@ -1,65 +1,31 @@
 import { ResumeCard } from "@/components/data/resume-card";
 import { BlurFade } from "@/components/magicui/blur-fade";
 
-const BLUR_FADE_DELAY = 0;
+import { FormatDate } from "@/lib/utils/common";
 
-const data = {
-  work: [
-    {
-      company: "Plum",
-      href: "https://withplum.com",
-      badges: [],
-      location: "On-site",
-      title: "Software Development Intern Trainee",
-      logoUrl: "/orgs/plum.jpeg",
-      start: "Sep 2024",
-      end: "Sep 2024",
-      description:
-        "Enhanced AI assistant performance by experimenting with embedding-based matching and LLM integrations (Langchain, OpenAI), identifying LLMs as optimal for rapid categorization.",
-    },
-    {
-      company: "HagoBuy",
-      href: "https://hagobuy.com",
-      badges: [],
-      location: "Remote",
-      title: "Affiliate Marketer (Level 5/5)",
-      logoUrl: "",
-      start: "Nov 2023",
-      end: "Jan 2024",
-      description:
-        " Generated over $150,000 in revenue for an ecommerce website in just 3 months, significantly expanding the platform’s presence in Greece as a top-performing affiliate.",
-    },
-    {
-      company: "efood",
-      href: "https://e-food.gr",
-      badges: [],
-      location: "On-site",
-      title: "Software Development Intern Trainee",
-      logoUrl: "/orgs/efood.jpeg",
-      start: "Jul 2023",
-      end: "Jul 2023",
-      description:
-        "Developed an AI recipe generator using Next.js and OpenAI's API, collaborating closely with the Frontend Team lead to refine web development skills and integrate project features effectively.",
-    },
-  ],
-};
+import { LinkedInProfile } from "@/types/proxycurl";
 
-export function Work() {
+export function Work({
+  linkedInProfile,
+}: {
+  linkedInProfile: LinkedInProfile;
+}) {
   return (
     <section id="work">
-      <BlurFade delay={BLUR_FADE_DELAY}>
+      <BlurFade>
         <div className="flex min-h-0 flex-col gap-y-3">
           <h2 className="text-xl font-bold">Work Experience</h2>
-          {data.work.map((work, id) => (
+          {linkedInProfile.experiences.map((work, id) => (
             <ResumeCard
               key={work.company}
-              logoUrl={work.logoUrl}
+              logoUrl={work.logo_url}
               altText={work.company}
               title={work.company}
               subtitle={work.title}
-              href={work.href}
-              badges={work.badges}
-              period={`${work.start} - ${work.end ?? "Present"}`}
+              href={work.company_linkedin_profile_url}
+              period={`${FormatDate(work.starts_at!)} - ${
+                work.ends_at ? `${FormatDate(work.ends_at)}` : "Present"
+              }`}
               description={work.description}
             />
           ))}
---
+++ src/lib/utils/common.ts
@@ -0,0 +1,9 @@
+import { months } from "@/lib/constants/common";
+
+export function FormatDate(date: {
+  year: number;
+  month: number;
+  day: number;
+}): string {
+  return `${months[date.month - 1]} ${date.year}`;
+}
